90a4277ea3cb5056ee2b6f495c5625e48f98890d
Ignacio Bisio <ibisio@gocloud.la>
2025-12-29 15:51:01 -0300

    feat(module): add custom security group

--- locals.tf
+++ locals.tf
@@ -1,9 +1,12 @@
 locals {
-  rds_vpc_security_group_ids = concat(
-    (
-      lookup(each.value, "security_group_create", true) ? [module.security_group_rds[each.key].security_group_id] : []
-    ),
-    lookup(each.value, "vpc_security_group_ids", []),
-    lookup(var.rds_aurora_defaults, "vpc_security_group_ids", [])
-  )
+  rds_vpc_security_group_ids = { 
+    for key, value in var.rds_aurora_parameters :
+    key => concat(
+      (
+        lookup(value, "security_group_create", true) ? [module.security_group_rds[key].security_group_id] : []
+      ),
+      lookup(value, "vpc_security_group_ids", []),
+      lookup(var.rds_aurora_defaults, "vpc_security_group_ids", [])
+    )
+  }
 }
