b381172b55b5fc2bd7d5498b7707df01f5c71f70
Ignacio Bisio <ibisio@gocloud.la>
2026-01-05 12:22:58 -0300

    fix(locals): change condition to security groups ids

--- locals.tf
+++ locals.tf
@@ -5,8 +5,10 @@ locals {
       (
         lookup(value, "security_group_create", true) ? [module.security_group_rds[key].security_group_id] : []
       ),
-      lookup(value, "vpc_security_group_ids", []),
-      lookup(var.rds_aurora_defaults, "vpc_security_group_ids", [])
+      try(coalescelist(
+        lookup(value, "vpc_security_group_ids", []),
+        lookup(var.rds_aurora_defaults, "vpc_security_group_ids", [])
+      ), [])
     )
   }
 }
